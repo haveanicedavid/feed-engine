c14f55537da745d23c46dc45c1579430ecaf0d85
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-04 15:23:58 -0600

    Adds data to tweet objects

--- app/models/search_results.rb
+++ app/models/search_results.rb
@@ -1,6 +1,6 @@
 class SearchResults
   attr_reader :service
-  
+
   def initialize(search)
     @search = search
   end
@@ -11,8 +11,16 @@ class SearchResults
 
   def all_tweets
     TwittersearchService.new.search_by(@search).attrs[:statuses].map do |tweet|
-      tweet[:text]
+      t = Tweet.new
+      t.text = tweet[:text]
+      t.created_at = tweet[:created_at]
+      t.screen_name = tweet[:user][:screen_name]
+      t
     end
   end
-  
+
+end
+
+class Tweet < OpenStruct
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :analyses, only: [:create, :index]
-  
+
   root 'static_pages#index'
-  
+
   get '/auth/twitter/callback', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
-  
+
   resources :searches, only: [:index, :show, :create]
-  
- # post '/results', to: "searches#search_results"
- # get '/results', to: "searches#search_results_display"
-  
+
+ post '/results', to: "searches#search_results"
+ get '/results', to: "searches#search_results_display"
+
 end

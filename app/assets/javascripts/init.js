fab09488f1d57c9f5818c0a761ea0f28bed67bd8
David Daniel <davedan7@gmail.com>
2015-06-08 18:52:44 -0600

    Ajax working well

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -15,7 +15,7 @@
       $.post("/results", { word: searchWord }).then(function(tweets){
         $tweets.empty();
         $tweets.append(tweets.map(function(tweet){
-          return $("<div class='col s12 m4'> <div class='card blue-grey darken-1 tweet-cards z-depth-3 display-cards'> <div class='card-content white-text'> <span class='card-title'>" + tweet.table.text + "</span> </div> </div> </div>");
+          return $("<div class='col s12 m4'> <div class='card blue-grey darken-1 tweet-cards z-depth-3 display-cards'> <div class='card-content white-text'> <span class='card-title'> <a href='https://twitter.com/" + tweet.table.screen_name + "' target='_blank'> @" + tweet.table.screen_name + "</a>" + "</span> <p> " + tweet.table.text + "</p> </div> <div class='card-action card-foot blue-text text-lighten-4'><h6> " + new Date(tweet.table.created_at).toLocaleDateString() + "</h6></div></div> </div>");
         }));
       })
       .fail(function() {

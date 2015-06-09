e4798b2533c599b88615c4bd64f93e9108a1065b
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-09 13:57:49 -0600

    finished email verification functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
-  
+  skip_before_action :check_email, :only => [:edit]
   def show
     @analyses = Analysis.where(user_id: current_user.id)
     @saved_searches = Search.where(user_id: current_user.id, active?: true)
+  end
+
+  def edit
+  end
+
+  def update
+    current_user.update(params[:email])
+    redirect_to user_path(current_user)
   end
 end

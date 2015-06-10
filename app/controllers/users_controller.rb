4b65e3c964bedbe1e4e5690cedfc1cd09b677f4e
David Daniel <davedan7@gmail.com>
2015-06-09 21:35:51 -0600

    View work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   
   def show
     @analyses = Analysis.where(user_id: current_user.id)
-    @saved_searches = Search.where(user_id: current_user.id, active?: true)
   end
+  
 end

e9d9d4301464ed1e59efaf0a493fa1e0e02164d0
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-09 22:12:59 -0600

    fixes usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
   def daily_digest_email(user)
     @user = user
     @searches = user.searches
-    mail(to: "patrickmedaugh@gmail.com", subject: "Twitter Analyses")
+    mail(to: user.email, subject: "Twitter Analyses")
   end
 end

f0a436355118c76b3f5f01e7b643f2d76cb22cad
David Daniel <davedan7@gmail.com>
2015-06-15 09:58:53 -0600

    Whatever

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -17,33 +17,33 @@
         $tweets.append("<div class='row'><div class='col s3 offset-s1'><a href='/analyses' class='btn cyan darken-2'> Analyze '" + searchWord +  "'</a></div></div>");
         $tweets.append(tweets.map(function(tweet){
           return $("<div class='col s12 m4'>" +
-                    "<div class='card blue-grey darken-1 tweet-cards z-depth-3 display-cards'>" +
-                    " <div class='card-content white-text'>" + 
-                    "<span class='card-title'> <a href='https://twitter.com/" +
-                    tweet.table.screen_name + 
-                    "' target='_blank'> @" + 
-                    tweet.table.screen_name + 
-                    "</a>" + "</span> <p> " + 
-                    tweet.table.text + 
-                    "</p> </div> <div class='card-action card-foot blue-text text-lighten-4'><h6> " + 
-                    new Date(tweet.table.created_at).toLocaleDateString() + 
-                    "</h6></div></div> </div>");
+                   "<div class='card blue-grey darken-1 tweet-cards z-depth-3 display-cards'>" +
+                   " <div class='card-content white-text'>" +
+                   "<span class='card-title'> <a href='https://twitter.com/" +
+          tweet.table.screen_name +
+            "' target='_blank'> @" +
+            tweet.table.screen_name +
+            "</a>" + "</span> <p> " +
+            tweet.table.text +
+            "</p> </div> <div class='card-action card-foot blue-text text-lighten-4'><h6> " +
+            new Date(tweet.table.created_at).toLocaleDateString() +
+            "</h6></div></div> </div>");
         }));
       })
       .fail(function() {
-        alert('Something went horribly wrong.')
+        alert('Something went horribly wrong.');
       });
 
     });
     // Scrollfire
-      var options = [
-          {selector: '#staggered-test', offset: 300, callback: 'Materialize.toast("OOOohhhh!!", 1500 )' },
-          // {selector: '#staggered-test', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )' },
-          {selector: '#staggered-test', offset: 250, callback: 'Materialize.showStaggeredList("#staggered-test")' },
-          // {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' }
-        ];
-        Materialize.scrollFire(options);
+    var options = [
+      {selector: '#staggered-test', offset: 300, callback: 'Materialize.toast("OOOohhhh!!", 1500 )' },
+      // {selector: '#staggered-test', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )' },
+      {selector: '#staggered-test', offset: 250, callback: 'Materialize.showStaggeredList("#staggered-test")' },
+      // {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' }
+    ];
+    Materialize.scrollFire(options);
 
-      $('.parallax').parallax(); // Handles parallax scrolling
+    $('.parallax').parallax(); // Handles parallax scrolling
   }); // end of document ready
 })(jQuery); // end of jQuery name space
--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,15 +1,15 @@
 class AnalysesController < ApplicationController
-  
+
   def index
-    word = session["word"]
-    tweets = SearchResults.new(word).all_tweets
+    word         = session["word"]
+    tweets       = SearchResults.new(word).all_tweets
     tweets_array = tweets.map { |tweet| tweet.text }
-    user_id = session["user_id"]
-    
-    analyze = AylienAnalyze.new.grouped_analysis(tweets_array)
-    
-    @analysis = Analysis.create(user_id: user_id, keyword: word, analysis: analyze)
-    
+    user_id      = session["user_id"]
+
+    analyze      = AylienAnalyze.new.grouped_analysis(tweets_array)
+
+    @analysis    = Analysis.create(user_id: user_id, keyword: word, analysis: analyze)
+
     redirect_to user_path(current_user)
   end
 end
